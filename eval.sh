8b901002fa565dc23c0ac7ca4416207e5a31abae
RobbenRibery <rundong.liu@outlook.com>
2024-12-24 13:59:44 +0000

    added the eval results

---
+++ eval.sh
@@ -0,0 +1,6 @@
+python examples/craftax/craftax_plr.py \
+    --use_accel \
+    --mode eval \
+    --checkpoint_directory checkpoints/moed-claude35-static-easy-hard/0  \
+    --checkpoint_to_eval 10 \
+    --eval_num_attempts 1
